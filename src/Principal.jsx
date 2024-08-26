0589ab047000da5da338d931b53f83d60fea9928
EzequielC-Dev <ezequiel34324l@gmail.com>
2024-08-26 03:08:16 -0300

    Correção e finalização dos sliders no main

--- src/Principal.jsx
+++ src/Principal.jsx
@@ -1,28 +1,59 @@
 import { useState } from "react";
+import "./css/animacoes.css";
+import "./css/fotos.css";
 import { Swiper, SwiperSlide } from "swiper/react";
-import './css/animacoes.css';
-import './css/fotos.css';
-
+import "swiper/css";
+import 'swiper/less/navigation';
+import 'swiper/less/pagination';
 
 function Principal() {
   return (
     <div>
-      <main className="flex object-cover fotos-main">
-
-        <Swiper>
+      <div className="fotos-main">
+        <Swiper
           slidesPerView={1}
-          pagination
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
           navigation
+          pagination={{clickable: true}}
+        >
           <SwiperSlide>
-            <img className="" src="./src/assets/Imagem Fundo Home 1.jpg" alt="Imagem fundo Home 1" id="1"/>
-            <img className="" src="./src/assets/Imagem fundo home 2.jpg" alt="Imagem fundo Home 2" id="2"/>
-            <img className="" src="./src/assets/Imagem fundo home 3.jpg" alt="Imagem fundo Home 3" id="3"/>
-            <img className="" src="./src/assets/Imagem fundo home 4.jpg" alt="Imagem fundo Home 4" id="4"/>
+            <img
+              className="brightness-50 "
+              src="./src/assets/Imagem Fundo Home 1.jpg"
+              alt="Imagem fundo Home 1"
+              id="1"
+            />
           </SwiperSlide>
-  
-          
+          <SwiperSlide>
+            <img
+              className="brightness-50 "
+              src="./src/assets/Imagem fundo home 2.jpg"
+              alt="Imagem fundo Home 2"
+              id="2"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className="brightness-50 "
+              src="./src/assets/Imagem fundo home 3.jpg"
+              alt="Imagem fundo Home 3"
+              id="3"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className="brightness-50 "
+              src="./src/assets/Imagem fundo home 4.jpg"
+              alt="Imagem fundo Home 4"
+              id="4"
+            />
+          </SwiperSlide>
         </Swiper>
-      </main>
+      </div>
+
+      <section>
+      </section>
     </div>
   );
 }
